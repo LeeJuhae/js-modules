9289c74e9f06bc52ee31bf9dff5a2418315b7212
LeeJuhae <tlatmsdlwngo@naver.com>
2021-06-13 00:45:50 +0900

    refactor: <time> 태그로 변경

--- timer/timer.js
+++ timer/timer.js
@@ -3,20 +3,20 @@ import { Utils } from './App.js';
 const Timer = function (goalTime) {
   this.goalTime = new Date(goalTime);
   this.countdown = () => {
-    const now = new Date(Date.now());
-    const ms = this.goalTime - now;
-    if (ms >= 0) {
-      const hours = parseInt(ms / 1000 / 60 / 60, 10);
-      const minutes = parseInt((ms / 1000 / 60) % 60, 10);
-      const seconds = parseInt((ms / 1000) % 60, 10);
+    const timer = document.getElementById('timer');
+    if (timer) {
+      const now = new Date(Date.now());
+      const ms = this.goalTime - now;
+      if (ms >= 0) {
+        const hours = parseInt(ms / 1000 / 60 / 60, 10);
+        const minutes = parseInt((ms / 1000 / 60) % 60, 10);
+        const seconds = parseInt((ms / 1000) % 60, 10);
 
-      let hour = document.getElementById('hour');
-      let minute = document.getElementById('minute');
-      let second = document.getElementById('second');
-
-      hour.textContent = Utils.setFormat(hours);
-      minute.textContent = Utils.setFormat(minutes);
-      second.textContent = Utils.setFormat(seconds);
+        const leftTime =
+          Utils.setFormat(hours) + ':' + Utils.setFormat(minutes) + ':' + Utils.setFormat(seconds);
+        timer.textContent = leftTime;
+        timer.setAttribute('datetime', leftTime);
+      }
     }
   };
 };
