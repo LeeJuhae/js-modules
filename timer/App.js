778fad8f6fbfd43fcf9a8184da327dfb00b24006
LeeJuhae <tlatmsdlwngo@naver.com>
2021-06-12 15:44:05 +0900

    refactor: 모듈화

---
+++ timer/App.js
@@ -0,0 +1,16 @@
+import Timer from './Timer.js';
+
+export const Utils = {
+  setFormat(time) {
+    return time < 10 ? '0' + time : time;
+  }
+};
+
+const App = () => {
+  const goalTime = '2021, 06, 13, 00:00';
+  const timer = new Timer(goalTime);
+  timer.countdown(); // 화면 로딩되자마자 시간 나오게 하기 위해 timer.countdown() 호출함.
+  setInterval(timer.countdown, 1000);
+};
+
+App();
--- timer/timer.js
+++ timer/timer.js
@@ -1,8 +1,4 @@
-const utils = {
-  setFormat(time) {
-    return time < 10 ? '0' + time : time;
-  }
-};
+import { Utils } from './App.js';
 
 const Timer = function (goalTime) {
   this.goalTime = new Date(goalTime);
@@ -18,18 +14,11 @@ const Timer = function (goalTime) {
       let minute = document.getElementById('minute');
       let second = document.getElementById('second');
 
-      hour.innerHTML = utils.setFormat(hours);
-      minute.innerHTML = utils.setFormat(minutes);
-      second.innerHTML = utils.setFormat(seconds);
+      hour.innerHTML = Utils.setFormat(hours);
+      minute.innerHTML = Utils.setFormat(minutes);
+      second.innerHTML = Utils.setFormat(seconds);
     }
   };
 };
 
-const App = () => {
-  const goalTime = '2021, 06, 12, 00:00';
-  const timer = new Timer(goalTime);
-  timer.countdown(); // 화면 로딩되자마자 시간 나오게 하기 위해 timer.countdown() 호출함.
-  setInterval(timer.countdown, 1000);
-};
-
-App();
+export default Timer;
